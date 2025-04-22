85a0d749016a520478b3b3f37bfcf14cd57852c0
k7aled <khd.rashidi@gmail.com>
2025-04-22 23:04:12 +0330

    init project

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// FILE: main.js
+
+import { createApp } from "vue";
+import { Quasar } from "quasar";
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
+import router from "./router"; // Import the router
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from "./App.vue";
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+
+myApp.use(router); // Tell Vue to use the router
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">registry-form</div>
+</template>
+
+<script></script>
